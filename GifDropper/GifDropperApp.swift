33dd7742213b2a0253c6e6d2c0fa633d13562322
Lavi Sharma <40620724+ilavisharma@users.noreply.github.com>
2026-05-31 14:14:26 +0530

    Initial Commit

---
+++ GifDropper/GifDropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GifDropperApp.swift
+//  GifDropper
+//
+//  Created by Lavi Sharma on 31/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GifDropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
